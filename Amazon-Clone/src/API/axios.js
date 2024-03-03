b31a68ca3d7907684222945f2a4aaf137643f1da
Alaz123 <meridalazar123@gmail.com>
2024-03-03 18:44:36 +0300

    back end code deploy on render & changing the baseurl

--- Amazon-Clone/src/API/axios.js
+++ Amazon-Clone/src/API/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	// local instace of firebase instance
-	baseURL: "http://127.0.0.1:5001/clone-93eda/us-central1/api",
-	// link is :- http://127.0.0.1:5001/clone-93eda/us-central1/api
-	// // deployed version of amazon server on render.com
-	// baseURL:"https://amazon-clone-backend-api-deploy.onrender.com"
+	// // local instace of firebase instance
+	// baseURL: " http://127.0.0.1:5001/clone-93eda/us-central1/api",
+
+	// deployed version of amazon server on render.com
+	baseURL: "https://amazon-server-side.onrender.com/",
 });
 
 export { axiosInstance };
